1aa4884ae7342b533e7109a43c9dc356f0c48100
Alicia Castaneda <ali.castaneda05@gmail.com>
2018-08-16 20:16:45 -0300

    My First Commit

---
+++ NewPoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewPoo
+//
+//  Created by Alicia on 8/16/18.
+//  Copyright © 2018 Alicia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
